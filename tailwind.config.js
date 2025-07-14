80f873ab0cc0ab2377e19afb96f5e75c68a95906
cassielx64 <soulcassiel@gmail.com>
2025-07-15 01:12:25 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#FFD700",
+      },
+      dropShadow: {
+        gold: "0 0 10px #FFD700",
+      },
+    },
+  },
+  plugins: [],
+};
